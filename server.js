de01d1babd9b7da46410d8a10f3aeabe480f0c16
Isaac Svedin <swedinisaac@gmail.com>
2025-11-23 15:37:16 +0100

    Fixed cors.

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const fastify = require("fastify")({ logger: true });
 
 require('dotenv').config();
 
+fastify.register(require("@fastify/cors"), {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+});
+
 fastify.register(require("./config/db"));
 fastify.register(require("./routes/item.route"));
 
